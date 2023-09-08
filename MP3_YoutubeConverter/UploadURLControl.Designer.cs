9b78601fc5261748a72a809ae1d33863da73d8b4
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-08 19:40:53 +0800

    SEPARATE FUNCTION TO RESIZE THE MAIN FORM

--- MP3_YoutubeConverter/UploadURLControl.Designer.cs
+++ MP3_YoutubeConverter/UploadURLControl.Designer.cs
@@ -30,50 +30,50 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UploadURLControl));
-            this.usernameBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.youtubeUrlTextBox = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.browseBtn = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
-            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
-            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.titleBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.thumbnailBox = new Guna.UI2.WinForms.Guna2PictureBox();
             this.guna2ShadowPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnailBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // usernameBox
+            // youtubeUrlTextBox
             // 
-            this.usernameBox.BackColor = System.Drawing.Color.Transparent;
-            this.usernameBox.BorderColor = System.Drawing.Color.DarkGray;
-            this.usernameBox.BorderRadius = 10;
-            this.usernameBox.BorderThickness = 2;
-            this.usernameBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.usernameBox.DefaultText = "";
-            this.usernameBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.usernameBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.usernameBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.usernameBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.usernameBox.FillColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.usernameBox.FocusedState.BorderColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.FocusedState.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.usernameBox.ForeColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.HoverState.BorderColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.HoverState.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.IconLeft = ((System.Drawing.Image)(resources.GetObject("usernameBox.IconLeft")));
-            this.usernameBox.IconLeftSize = new System.Drawing.Size(25, 25);
-            this.usernameBox.Location = new System.Drawing.Point(15, 46);
-            this.usernameBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.usernameBox.Name = "usernameBox";
-            this.usernameBox.PasswordChar = '\0';
-            this.usernameBox.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
-            this.usernameBox.PlaceholderText = "Paste Youtube link";
-            this.usernameBox.SelectedText = "";
-            this.usernameBox.Size = new System.Drawing.Size(650, 44);
-            this.usernameBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
-            this.usernameBox.TabIndex = 4;
-            this.usernameBox.TextOffset = new System.Drawing.Point(10, 0);
+            this.youtubeUrlTextBox.BackColor = System.Drawing.Color.Transparent;
+            this.youtubeUrlTextBox.BorderColor = System.Drawing.Color.DarkGray;
+            this.youtubeUrlTextBox.BorderRadius = 10;
+            this.youtubeUrlTextBox.BorderThickness = 2;
+            this.youtubeUrlTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.youtubeUrlTextBox.DefaultText = "";
+            this.youtubeUrlTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.youtubeUrlTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.youtubeUrlTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.youtubeUrlTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.youtubeUrlTextBox.FillColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.youtubeUrlTextBox.FocusedState.BorderColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.FocusedState.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
+            this.youtubeUrlTextBox.ForeColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.HoverState.BorderColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.HoverState.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.IconLeft = ((System.Drawing.Image)(resources.GetObject("youtubeUrlTextBox.IconLeft")));
+            this.youtubeUrlTextBox.IconLeftSize = new System.Drawing.Size(25, 25);
+            this.youtubeUrlTextBox.Location = new System.Drawing.Point(15, 46);
+            this.youtubeUrlTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.youtubeUrlTextBox.Name = "youtubeUrlTextBox";
+            this.youtubeUrlTextBox.PasswordChar = '\0';
+            this.youtubeUrlTextBox.PlaceholderForeColor = System.Drawing.Color.SteelBlue;
+            this.youtubeUrlTextBox.PlaceholderText = "Paste Youtube link";
+            this.youtubeUrlTextBox.SelectedText = "";
+            this.youtubeUrlTextBox.Size = new System.Drawing.Size(650, 44);
+            this.youtubeUrlTextBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.youtubeUrlTextBox.TabIndex = 4;
+            this.youtubeUrlTextBox.TextOffset = new System.Drawing.Point(10, 0);
             // 
             // guna2HtmlLabel1
             // 
@@ -112,50 +112,14 @@
             // 
             this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2ShadowPanel1.Controls.Add(this.guna2Button2);
-            this.guna2ShadowPanel1.Controls.Add(this.guna2TextBox1);
-            this.guna2ShadowPanel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2ShadowPanel1.Controls.Add(this.titleBox);
+            this.guna2ShadowPanel1.Controls.Add(this.thumbnailBox);
             this.guna2ShadowPanel1.FillColor = System.Drawing.Color.SteelBlue;
             this.guna2ShadowPanel1.Location = new System.Drawing.Point(61, 127);
             this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
             this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
             this.guna2ShadowPanel1.Size = new System.Drawing.Size(680, 261);
             this.guna2ShadowPanel1.TabIndex = 7;
-            // 
-            // guna2PictureBox1
-            // 
-            this.guna2PictureBox1.BorderRadius = 5;
-            this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(25, 20);
-            this.guna2PictureBox1.Name = "guna2PictureBox1";
-            this.guna2PictureBox1.Size = new System.Drawing.Size(320, 180);
-            this.guna2PictureBox1.TabIndex = 0;
-            this.guna2PictureBox1.TabStop = false;
-            // 
-            // guna2TextBox1
-            // 
-            this.guna2TextBox1.BorderThickness = 0;
-            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox1.DefaultText = "How to Get YouTube API Key 2023 | Create YouTube API Key ( YouTube Data API v3 )";
-            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.FillColor = System.Drawing.Color.SteelBlue;
-            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2TextBox1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Location = new System.Drawing.Point(366, 62);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.guna2TextBox1.Multiline = true;
-            this.guna2TextBox1.Name = "guna2TextBox1";
-            this.guna2TextBox1.PasswordChar = '\0';
-            this.guna2TextBox1.PlaceholderText = "";
-            this.guna2TextBox1.ReadOnly = true;
-            this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(285, 122);
-            this.guna2TextBox1.TabIndex = 1;
-            this.guna2TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // guna2Button2
             // 
@@ -174,6 +138,43 @@
             this.guna2Button2.TabIndex = 7;
             this.guna2Button2.Text = "Convert";
             // 
+            // titleBox
+            // 
+            this.titleBox.BorderThickness = 0;
+            this.titleBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.titleBox.DefaultText = "How to Get YouTube API Key 2023 | Create YouTube API Key ( YouTube Data API v3 )";
+            this.titleBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.titleBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.titleBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.titleBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.titleBox.FillColor = System.Drawing.Color.SteelBlue;
+            this.titleBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.titleBox.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleBox.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.titleBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.titleBox.Location = new System.Drawing.Point(366, 62);
+            this.titleBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.titleBox.Multiline = true;
+            this.titleBox.Name = "titleBox";
+            this.titleBox.PasswordChar = '\0';
+            this.titleBox.PlaceholderText = "";
+            this.titleBox.ReadOnly = true;
+            this.titleBox.SelectedText = "";
+            this.titleBox.Size = new System.Drawing.Size(285, 122);
+            this.titleBox.TabIndex = 1;
+            this.titleBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // thumbnailBox
+            // 
+            this.thumbnailBox.BorderRadius = 5;
+            this.thumbnailBox.ImageRotate = 0F;
+            this.thumbnailBox.Location = new System.Drawing.Point(25, 20);
+            this.thumbnailBox.Name = "thumbnailBox";
+            this.thumbnailBox.Size = new System.Drawing.Size(320, 180);
+            this.thumbnailBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.thumbnailBox.TabIndex = 0;
+            this.thumbnailBox.TabStop = false;
+            // 
             // UploadURLControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -182,11 +183,11 @@
             this.Controls.Add(this.guna2ShadowPanel1);
             this.Controls.Add(this.browseBtn);
             this.Controls.Add(this.guna2HtmlLabel1);
-            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.youtubeUrlTextBox);
             this.Name = "UploadURLControl";
             this.Size = new System.Drawing.Size(803, 406);
             this.guna2ShadowPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnailBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,13 +195,13 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2TextBox usernameBox;
+        private Guna.UI2.WinForms.Guna2TextBox youtubeUrlTextBox;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private Guna.UI2.WinForms.Guna2Button browseBtn;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
-        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox titleBox;
+        private Guna.UI2.WinForms.Guna2PictureBox thumbnailBox;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
     }
 }
--- MP3_YoutubeConverter/UploadURLControl.cs
+++ MP3_YoutubeConverter/UploadURLControl.cs
@@ -3,21 +3,65 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 
 namespace MP3_YoutubeConverter
 {
     public partial class UploadURLControl : UserControl
     {
+        private readonly YouTubeService youtubeService;
+
         public UploadURLControl()
         {
             InitializeComponent();
+
+            // Initialize the YouTubeService with your API Key
+            youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDAuK-6Fyeper2Z55yEu88C-kEXdm-i4I4"
+            });
         }
 
+        #region FUNCTION TO BROWSE THE YOUTUBE URL
+
         private void browseBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(youtubeUrlTextBox.Text))
+            {
+                MessageBox.Show("Please enter the required URL.");
+                return;
+            }
+
+            // Get the YouTube video URL from the text box.
+            string videoUrl = youtubeUrlTextBox.Text;
+
+            // Extract the video ID from the URL.
+            string videoId = GetVideoId(videoUrl);
+
+            if (string.IsNullOrEmpty(videoId))
+            {
+                MessageBox.Show("Invalid YouTube URL.");
+                return;
+            }
+
+            ResizeMainForm();
+
+            AccessTheYoutubeService(videoId);
+        }
+
+        #endregion
+
+        #region FUNCTION TO RESIZE THE MAIN FORM
+
+        private void ResizeMainForm()
         {
             // Get a reference to the MainForm if it exists.
             MainForm mainForm = Application.OpenForms.OfType<MainForm>().FirstOrDefault();
@@ -33,8 +77,91 @@ namespace MP3_YoutubeConverter
                  );
                 // Perform layout if needed.
                 mainForm.PerformLayout();
-
             }
         }
+
+        #endregion
+
+        private async void AccessTheYoutubeService(string videoId)
+        {
+            // Check if the video ID is not empty.
+            if (!string.IsNullOrEmpty(videoId))
+            {
+                var videoRequest = youtubeService.Videos.List("snippet");
+                videoRequest.Id = videoId;
+
+                var videoResponse = await videoRequest.ExecuteAsync();
+
+                if (videoResponse.Items.Count > 0)
+                {
+                    string thumbnailUrl = videoResponse.Items[0].Snippet.Thumbnails.Maxres.Url;
+                    string videoTitle = videoResponse.Items[0].Snippet.Title;
+
+                    await LoadImageAsync(thumbnailUrl);
+
+                    // Display the title and duration
+                    titleBox.Text = videoTitle;
+                }
+            }
+            else
+                MessageBox.Show("Invalid YouTube URL.");
+        }
+
+        private string GetVideoId(string url)
+        {
+            // Extract the video ID from the YouTube URL
+            if (url.Contains("youtube.com"))
+            {
+                try
+                {
+                    var uri = new Uri(url);
+                    var query = uri.Query;
+                    var videoId = System.Web.HttpUtility.ParseQueryString(query).Get("v");
+                    return videoId;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else if (url.Contains("youtu.be"))
+            {
+                try
+                {
+                    var uri = new Uri(url);
+                    return uri.Segments[1];
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        private async Task LoadImageAsync(string thumbnailUrl)
+        {
+            // Assuming thumbnailUrl is the URL of the retrieved thumbnail
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    byte[] imageBytes = await webClient.DownloadDataTaskAsync(thumbnailUrl);
+
+                    using (var memoryStream = new MemoryStream(imageBytes))
+                    {
+                        // Load the image into a temporary variable
+                        var tempImage = Image.FromStream(memoryStream);
+
+                        thumbnailBox.Image = tempImage;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to load image: {ex.Message}");
+                }
+            }
+        }
+
     }
 }
