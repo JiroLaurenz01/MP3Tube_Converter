ead916ea281166c29bf74eca6369639aa82d6027
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-08 20:33:26 +0800

    DESIGN FOR LOADING SCREEN FORM

---
+++ MP3_YoutubeConverter/LoadingScreenForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MP3_YoutubeConverter
+{
+    partial class LoadingScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2WinProgressIndicator1 = new Guna.UI2.WinForms.Guna2WinProgressIndicator();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // guna2WinProgressIndicator1
+            // 
+            this.guna2WinProgressIndicator1.AnimationSpeed = 65;
+            this.guna2WinProgressIndicator1.AutoStart = true;
+            this.guna2WinProgressIndicator1.CircleSize = 9.5F;
+            this.guna2WinProgressIndicator1.Location = new System.Drawing.Point(25, 25);
+            this.guna2WinProgressIndicator1.Name = "guna2WinProgressIndicator1";
+            this.guna2WinProgressIndicator1.ProgressColor = System.Drawing.Color.White;
+            this.guna2WinProgressIndicator1.Size = new System.Drawing.Size(150, 150);
+            this.guna2WinProgressIndicator1.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // LoadingScreenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(200, 200);
+            this.Controls.Add(this.guna2WinProgressIndicator1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoadingScreenForm";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FlashingScreenForm";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Desktop;
+            this.Load += new System.EventHandler(this.LoadingScreenForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2WinProgressIndicator guna2WinProgressIndicator1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
