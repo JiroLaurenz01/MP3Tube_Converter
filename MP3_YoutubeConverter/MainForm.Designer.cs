936de19d835ce6c10814e02318594a4b73042286
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-08 18:52:50 +0800

    UPDATED DESIGN FOR UploadURLControl, IT INCLUDES THE YOUTUBE THUMBNAIL AND TITLE

--- MP3_YoutubeConverter/MainForm.Designer.cs
+++ MP3_YoutubeConverter/MainForm.Designer.cs
@@ -34,11 +34,11 @@
             this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.guna2ContainerControl1 = new Guna.UI2.WinForms.Guna2ContainerControl();
             this.mainPanel = new Guna.UI2.WinForms.Guna2Panel();
-            this.panelContainer = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.panelContainer = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2ContainerControl1.SuspendLayout();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
@@ -93,51 +93,8 @@
             this.mainPanel.FillColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.mainPanel.Location = new System.Drawing.Point(0, 31);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(827, 436);
+            this.mainPanel.Size = new System.Drawing.Size(827, 272);
             this.mainPanel.TabIndex = 1;
-            // 
-            // panelContainer
-            // 
-            this.panelContainer.BackColor = System.Drawing.Color.Transparent;
-            this.panelContainer.BorderRadius = 10;
-            this.panelContainer.FillColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panelContainer.Location = new System.Drawing.Point(12, 141);
-            this.panelContainer.Name = "panelContainer";
-            this.panelContainer.Size = new System.Drawing.Size(803, 283);
-            this.panelContainer.TabIndex = 0;
-            // 
-            // guna2HtmlLabel1
-            // 
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(204, 22);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(202, 56);
-            this.guna2HtmlLabel1.TabIndex = 1;
-            this.guna2HtmlLabel1.Text = "MP3Tube";
-            // 
-            // guna2HtmlLabel2
-            // 
-            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(412, 22);
-            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            this.guna2HtmlLabel2.Size = new System.Drawing.Size(210, 56);
-            this.guna2HtmlLabel2.TabIndex = 2;
-            this.guna2HtmlLabel2.Text = "Converter";
-            // 
-            // guna2CustomGradientPanel1
-            // 
-            this.guna2CustomGradientPanel1.FillColor = System.Drawing.SystemColors.MenuHighlight;
-            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.SystemColors.HotTrack;
-            this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.SystemColors.GradientActiveCaption;
-            this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.SystemColors.ActiveCaption;
-            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(207, 73);
-            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
-            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(415, 5);
-            this.guna2CustomGradientPanel1.TabIndex = 0;
             // 
             // guna2HtmlLabel3
             // 
@@ -150,11 +107,54 @@
             this.guna2HtmlLabel3.TabIndex = 0;
             this.guna2HtmlLabel3.Text = "Youtube to MP3 Converter";
             // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.SystemColors.MenuHighlight;
+            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.SystemColors.HotTrack;
+            this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.SystemColors.GradientActiveCaption;
+            this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.SystemColors.ActiveCaption;
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(207, 73);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(415, 5);
+            this.guna2CustomGradientPanel1.TabIndex = 0;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(412, 22);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(210, 56);
+            this.guna2HtmlLabel2.TabIndex = 2;
+            this.guna2HtmlLabel2.Text = "Converter";
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(204, 22);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(202, 56);
+            this.guna2HtmlLabel1.TabIndex = 1;
+            this.guna2HtmlLabel1.Text = "MP3Tube";
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.Transparent;
+            this.panelContainer.BorderRadius = 10;
+            this.panelContainer.FillColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panelContainer.Location = new System.Drawing.Point(12, 141);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(803, 118);
+            this.panelContainer.TabIndex = 0;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 467);
+            this.ClientSize = new System.Drawing.Size(827, 303);
             this.Controls.Add(this.mainPanel);
             this.Controls.Add(this.guna2ContainerControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
