d9e64bc1ba67f4d53c66497ccc3d306985d84d9b
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-09 23:24:33 +0800

    UPDATE DESIGN FOR USER CONTROL OF ConvertMP3Control

--- MP3_YoutubeConverter/ConvertMP3Control.Designer.cs
+++ MP3_YoutubeConverter/ConvertMP3Control.Designer.cs
@@ -41,16 +41,13 @@
             this.convertAgainBtn.BackColor = System.Drawing.Color.Transparent;
             this.convertAgainBtn.BorderRadius = 5;
             this.convertAgainBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.convertAgainBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.convertAgainBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.convertAgainBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.convertAgainBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.convertAgainBtn.Enabled = false;
             this.convertAgainBtn.FillColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.convertAgainBtn.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F);
             this.convertAgainBtn.ForeColor = System.Drawing.Color.SteelBlue;
             this.convertAgainBtn.Location = new System.Drawing.Point(3, 1);
-            this.convertAgainBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.convertAgainBtn.Margin = new System.Windows.Forms.Padding(4);
             this.convertAgainBtn.Name = "convertAgainBtn";
             this.convertAgainBtn.Size = new System.Drawing.Size(131, 43);
             this.convertAgainBtn.TabIndex = 8;
@@ -67,9 +64,9 @@
             this.progressBar.BorderRadius = 8;
             this.progressBar.BorderThickness = 2;
             this.progressBar.Location = new System.Drawing.Point(85, 91);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
             this.progressBar.Name = "progressBar";
-            this.progressBar.ProgressColor = System.Drawing.Color.SteelBlue;
+            this.progressBar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(153)))), ((int)(((byte)(230)))));
             this.progressBar.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(153)))), ((int)(((byte)(230)))));
             this.progressBar.Size = new System.Drawing.Size(900, 37);
             this.progressBar.TabIndex = 9;
@@ -83,7 +80,7 @@
             this.loadingPercent.Font = new System.Drawing.Font("Bahnschrift Light", 16F);
             this.loadingPercent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(153)))), ((int)(((byte)(230)))));
             this.loadingPercent.Location = new System.Drawing.Point(85, 55);
-            this.loadingPercent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loadingPercent.Margin = new System.Windows.Forms.Padding(4);
             this.loadingPercent.Name = "loadingPercent";
             this.loadingPercent.Size = new System.Drawing.Size(35, 27);
             this.loadingPercent.TabIndex = 11;
@@ -95,7 +92,7 @@
             this.statusLabel.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.statusLabel.ForeColor = System.Drawing.Color.SteelBlue;
             this.statusLabel.Location = new System.Drawing.Point(436, 21);
-            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4);
             this.statusLabel.Name = "statusLabel";
             this.statusLabel.Size = new System.Drawing.Size(146, 31);
             this.statusLabel.TabIndex = 13;
@@ -110,7 +107,7 @@
             this.Controls.Add(this.loadingPercent);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.convertAgainBtn);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ConvertMP3Control";
             this.Size = new System.Drawing.Size(1071, 145);
             this.Load += new System.EventHandler(this.ConvertMP3Control_Load);
