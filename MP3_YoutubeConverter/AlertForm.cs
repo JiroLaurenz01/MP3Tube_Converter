c5b447f52201304d0923ab695c8ce278106c8b6f
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-11 18:01:49 +0800

    FIELDS AND ENUMS FOR ALERT FORM

--- MP3_YoutubeConverter/AlertForm.cs
+++ MP3_YoutubeConverter/AlertForm.cs
@@ -1,20 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace MP3_YoutubeConverter
 {
     public partial class AlertForm : Form
     {
+        #region FIELDS AND ENUMS
+
+        private int x, y;
+
+        // Defines a public enumeration named Action with three possible values: wait, start, and close.
+        // This enumeration represents different states of the alert form.
+        public enum Action
+        {
+            wait,
+            start,
+            close
+        }
+
+        // Defines a public enumeration named Type with four possible values: Success, Warning, Error, and Info.
+        // This enumeration represents different types of alerts.
+        public enum Type
+        {
+            Success,
+            Warning,
+            Error,
+            Info
+        }
+
+        private AlertForm.Action action;
+
+        #endregion
+
         public AlertForm()
         {
             InitializeComponent();
         }
     }
 }
+
